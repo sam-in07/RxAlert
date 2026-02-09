61bf0ac5e465001584b327e1b7147066f094a999
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-09 17:42:16 +0600

    commit homePage

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -10,6 +10,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text(
+            'Worry less. \nLive healthier.',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Text(
+            'Welcome to Daily Dose.',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          Text(
+            '0',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      ),
+    );
   }
 }
