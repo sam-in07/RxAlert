3adb734651d878f905ace91cb5690479d17e76db
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-14 19:14:24 +0600

    commmit ye lo

--- lib/widgets/TopContainer.dart
+++ lib/widgets/TopContainer.dart
@@ -1,28 +1,39 @@
-
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
-class Topcontainer extends StatelessWidget{
+class Topcontainer extends StatelessWidget {
   const Topcontainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return Column(
-     children: [
-       Text(
-         'Worry less. \nLive healthier.',
-         style: Theme.of(context).textTheme.headlineMedium,
-       ),
-       Text(
-         'Welcome to Daily Dose.',
-         style: Theme.of(context).textTheme.titleSmall,
-       ),
-       Text(
-         '0',
-         style: Theme.of(context).textTheme.headlineMedium,
-       ),
-     ],
-   );
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(bottom: 1.h),
+          child: Text(
+            'Worry less. \nLive healthier.',
+            textAlign: TextAlign.start,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          padding: EdgeInsets.only(bottom: 1.h),
+          child: Text(
+            'Welcome to Daily Dose.',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
+        SizedBox(height: 2.h),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(bottom: 1.h),
+        ),
+        Text('0', style: Theme.of(context).textTheme.headlineMedium),
+      ],
+    );
   }
-
 }
