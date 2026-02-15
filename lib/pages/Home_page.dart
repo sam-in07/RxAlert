77533c6c4210be44183523b991d82b90f43a731e
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-15 08:48:13 +0600

    commmit ye lo

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 import '../widgets/TopContainer.dart';
 
@@ -14,10 +15,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Topcontainer(),
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(2.h),
+        child: Column(children: [Topcontainer()]),
       ),
     );
   }
