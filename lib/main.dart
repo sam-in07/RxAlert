9559e65765f79e547292eb97504dd1600a1c81fa
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-15 22:20:49 +0600

    commmit ye lo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rx_alert/pages/Home_page.dart';
+import 'package:rx_alert/random_dartss.dart';
+import 'package:rx_alert/widgets/colors.dart';
+import 'package:sizer/sizer.dart'; // Make sure this is imported
 
 void main() {
   runApp(const MyApp());
@@ -8,18 +12,35 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RxAlrt ',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const HomePage(),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          title: 'RxAlrt',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.dark().copyWith(
+            primaryColor: kPrimaryColor,
+            scaffoldBackgroundColor: kScaffoldColor,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: kScaffoldColor,
+              elevation: 0,
+            ),
+            textTheme: TextTheme(
+              headlineMedium: TextStyle(
+                fontSize: 24.sp,
+                fontWeight: FontWeight.w900,
+                color: kTextColor,
+              ),
+              titleSmall: GoogleFonts.poppins(
+                fontSize: 12.sp,
+                color: kTextLightColor,
+              ),
+            ),
+          ),
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
-
---
+++ lib/random_dartss.dart
@@ -0,0 +1,22 @@
+// Sizer(builder: (context, orientation, deviceType) {
+// return MaterialApp(
+// title: 'RxAlrt ',
+// debugShowCheckedModeBanner: false,
+// theme: ThemeData.dark().copyWith (
+// primaryColor: kPrimaryColor,
+// scaffoldBackgroundColor: kScaffoldColor,
+// // colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+// //appbar theme
+// appBarTheme: const AppBarTheme(
+// backgroundColor: kScaffoldColor,
+// elevation: 0,
+// ),
+// textTheme: TextTheme(
+//
+// )
+//
+// ),
+// home: const HomePage(),
+// );
+//
+// }
