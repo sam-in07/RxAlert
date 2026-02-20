1fd4a1d730edfceae68c53b9ee8086f3389fab58
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-20 19:28:32 +0600

    commit home page update

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -28,20 +28,25 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      floatingActionButton: SizedBox(
-        width: 19.w,
-        height: 9.h,
-        child: Card(
+      floatingActionButton: InkResponse(
+        onTap: () {
+          //go to next page
+        },
+        child: SizedBox(
+          width: 19.w,
+          height: 9.h,
+          child: Card(
 
-          color: kPrimaryColor,
-          shape: BeveledRectangleBorder(
-            borderRadius: BorderRadius.circular(3.h),
+            color: kPrimaryColor,
+            shape: BeveledRectangleBorder(
+              borderRadius: BorderRadius.circular(3.h),
 
-          ),
-          child: Icon(
-            Icons.add_outlined,
-            color: kScaffoldColor,
-            size: 50.sp,
+            ),
+            child: Icon(
+              Icons.add_outlined,
+              color: kScaffoldColor,
+              size: 50.sp,
+            ),
           ),
         ),
       ),
